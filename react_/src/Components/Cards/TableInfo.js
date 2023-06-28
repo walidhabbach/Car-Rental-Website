1a4d19fc714a109b90bacbe2c32b014e668826ad
walid habbach <79254928+walidhabbach@users.noreply.github.com>
2023-06-28 23:10:06 +0100

    Update Reservation Confirmation form

--- react_/src/Components/Cards/TableInfo.js
+++ react_/src/Components/Cards/TableInfo.js
@@ -1,4 +1,4 @@
-import React, { useEffect,useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBattery, faC, faChair, faGear, faPowerOff } from '@fortawesome/free-solid-svg-icons';
@@ -10,40 +10,40 @@ import Specs from './Specs';
 import { faDoorOpen } from '@fortawesome/free-solid-svg-icons';
 import Modal from 'react-modal'
 import ReservationForm from './ReservationForm';
-export default function TableInfo({data,carburant_type,gear_box}) {
+export default function TableInfo({ data, carburant_type, gear_box }) {
 
-  const [carburant,setCarburant] = useState("");
-  const [modalIsOpen,setModalIsOpen] = useState(false);
+  const [carburant, setCarburant] = useState("");
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
-  
+
   const specs_ = [
-     { icon: faGear, title: 'Gearbox',value: gear_box,color: 'text-cyan-500'},
-     { icon: faDoorOpen, title: 'Doors',value: data.doors,color: 'text-yellow-500'},
-     { icon: faCarBattery, title: 'Power',value: data.power,color: 'text-red-500'},
-     { icon: faChair, title: 'seats',value: data.seats,color: 'text-green-500'},
-     { icon: faGasPump, title: 'Fuel type',value: carburant_type,color: 'text-blue-500'},
+    { icon: faGear, title: 'Gearbox', value: gear_box, color: 'text-cyan-500' },
+    { icon: faDoorOpen, title: 'Doors', value: data.doors, color: 'text-yellow-500' },
+    { icon: faCarBattery, title: 'Power', value: data.power, color: 'text-red-500' },
+    { icon: faChair, title: 'seats', value: data.seats, color: 'text-green-500' },
+    { icon: faGasPump, title: 'Fuel type', value: carburant_type, color: 'text-blue-500' },
   ]
   return (
-        <div className='lg:w-full lg:px-12'>
-             <h3 className='font-bold text-4xl mb-5 mt-5'>Principal Specs</h3>
-                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6  '>
-                        {
-                            specs_.map((spec,index)=>{
-                                return(
-                                    <Specs
-                                      key={index}
-                                      icon={spec.icon}
-                                      title={spec.title}
-                                      value={spec.value}
-                                      color={spec.color}
-                                    />
-                                )
-                            })
-                        }
-                </div>
-                
-                <ReservationForm data={data}/>           
-                
-        </div>
+    <div className='lg:w-full lg:px-12'>
+      <h3 className='font-bold text-4xl mb-5 mt-5'>Principal Specs</h3>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6  '>
+        {
+          specs_.map((spec, index) => {
+            return (
+              <Specs
+                key={index}
+                icon={spec.icon}
+                title={spec.title}
+                value={spec.value}
+                color={spec.color}
+              />
+            )
+          })
+        }
+      </div>
+
+      <ReservationForm data={data} />
+
+    </div>
   )
 }
